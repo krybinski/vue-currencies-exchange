f91ae357f17908bf20da692fe1c455e918c021c5
Kamil Rybiński <kamilryba19@gmail.com>
2025-01-12 21:18:14 +0100

    heading component

---
+++ src/components/common/TheHeading.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface Props {
+  title: string;
+  tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  center?: boolean;
+}
+
+const { title, tag = 'h1', center = false } = defineProps<Props>();
+
+const textSizeClass = computed(() => ({
+  'text-2xl': tag === 'h1',
+  'text-xl': tag === 'h2',
+  'text-lg': tag === 'h3',
+  'text-base': tag === 'h4',
+  'text-sm': tag === 'h5',
+  'text-xs': tag === 'h6',
+}));
+</script>
+
+<template>
+  <component :is="tag" :class="{ 'text-center': center, ...textSizeClass }" class="font-bold">
+    {{ title }}
+  </component>
+</template>
+
+<style scoped></style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import TheContainer from '@/components/layouts/TheContainer.vue';
+import TheHeading from '@/components/common/TheHeading.vue';
+import TheContainer from '@/layouts/TheContainer.vue';
 </script>
 
 <template>
   <main class="w-full">
-    <TheContainer></TheContainer>
+    <TheContainer>
+      <TheHeading title="Currency Converter" :center="true" />
+    </TheContainer>
   </main>
 </template>
