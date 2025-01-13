611e67979e58824c3c95ed8bf2e1756256c02392
Kamil Rybiński <kamilryba19@gmail.com>
2025-01-13 20:35:13 +0100

    add Loader and Error Message

---
+++ src/components/common/ErrorMessage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface Props {
+  error: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="text-red-500">{{ error }}</div>
+</template>
+
+<style scoped></style>
--- src/components/common/TheDivider.vue
+++ src/components/common/TheDivider.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="w-full h-px bg-gray-300 my-6"></div>
+  <div class="w-full h-px bg-gray-200"></div>
 </template>
 
 <style scoped></style>
