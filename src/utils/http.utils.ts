3fa2a3e649d7aebe45e03b0ec0285e53f79c51e7
Kamil Rybiński <kamilryba19@gmail.com>
2025-01-13 16:11:12 +0100

    getErrorMessage util

---
+++ src/utils/http.utils.ts
@@ -0,0 +1,24 @@
+import { isAxiosError, type AxiosError } from 'axios';
+
+export function getErrorMessage(error: unknown, defaultError = ''): string {
+  if (error instanceof Error) {
+    return error.message;
+  }
+
+  if (isAxiosError(error)) {
+    const axiosError = error as AxiosError;
+
+    if (axiosError.response) {
+      return (
+        (axiosError.response.data as { message?: string })?.message ||
+        `Error ${axiosError.response.status}: ${axiosError.response.statusText}`
+      );
+    }
+
+    if (axiosError.request) {
+      return 'No response received from server';
+    }
+  }
+
+  return defaultError || 'An unknown error occurred';
+}
