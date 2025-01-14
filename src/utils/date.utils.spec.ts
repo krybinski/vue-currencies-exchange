b05e17865e1dd08754e0b5bc05f7e5e37628addb
Kamil Rybiński <kamilryba19@gmail.com>
2025-01-14 17:24:09 +0100

    fix test

--- src/utils/date.utils.spec.ts
+++ src/utils/date.utils.spec.ts
@@ -3,6 +3,6 @@ import { isValidDateFormat } from './date.utils';
 
 describe('isValidDateFormat', () => {
   it('should return true for valid date format', () => {
-    expect(isValidDateFormat('2024-1-01')).toBe(true);
+    expect(isValidDateFormat('2024-01-01')).toBe(true);
   });
 });
