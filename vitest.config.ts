bee6e11797680611a047ba094c975c9c733640ef
Kamil Rybiński <kamilryba19@gmail.com>
2025-01-14 21:07:11 +0100

    update vitest excluded files config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,15 +2,32 @@ import { fileURLToPath } from 'node:url';
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
 import viteConfig from './vite.config';
 
+const excludedFiles = [
+  ...configDefaults.exclude,
+  'env.*',
+  'postcss.config.js',
+  'tailwind.config.js',
+  'e2e/**',
+  'src/constants.ts',
+  'src/main.ts',
+  'src/api/*.ts',
+  'src/api/types/**',
+  'src/router/**',
+  'src/types/**',
+];
+
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
       environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
+      exclude: excludedFiles,
       root: fileURLToPath(new URL('./', import.meta.url)),
       outputFile: {
         json: 'coverage/report.json',
+      },
+      coverage: {
+        exclude: excludedFiles,
       },
     },
   }),
